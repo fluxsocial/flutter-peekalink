3a8a6a880092a568c7068edccf08f9516fb74efa
Fayeed Pawaskar <fayeed@live.com>
2021-03-01 14:19:52 +0530

    Removed all the embedly widgets

--- lib/embedly_preview.dart
+++
@@ -1,6 +0,0 @@
-library embedly_preview;
-
-export './models/embedly_response_model.dart';
-export './widgets/card_section.dart';
-export './widgets/rich_preview.dart';
-export './widgets/oembed_widget.dart';
